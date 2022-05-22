fb8ac9305b9c44f459cc34bd4f04fe646fd1ffac
jahangir <98384389+Jahangiresh@users.noreply.github.com>
2022-05-22 18:28:18 +0400

    final commit 5

--- jsFinal/main.js
+++ jsFinal/main.js
@@ -95,8 +95,43 @@ $('#owl4').owlCarousel({
             items:1
         },
         1000:{
-            items:1.3
+            items:1.4
         }
     }
 })
+
+$('#owl5').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+$('#owl6').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
 
