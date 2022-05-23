5b5ce9a33b51fdf6e87dabc9d7866e0b5110b760
jahangir <98384389+Jahangiresh@users.noreply.github.com>
2022-05-23 19:15:38 +0400

    jsFinal added 7

--- jsFinal/product.js
+++ jsFinal/product.js
@@ -1,40 +1,8 @@
-(function() {
-    const parent = document.querySelector('.range-slider');
-
-    if (!parent) {
-        return;
-    }
-
-    const rangeS = parent.querySelectorAll('input[type="range"]'),
-          numberS = parent.querySelectorAll('input[type="number"]');
-
-    rangeS.forEach((el) => {
-        el.oninput = () => {
-            let slide1 = parseFloat(rangeS[0].value),
-                slide2 = parseFloat(rangeS[1].value);
-
-            if (slide1 > slide2) {
-                [slide1, slide2] = [slide2, slide1];
-            }
-
-            numberS[0].value = slide1;
-            numberS[1].value = slide2;
-        }
-    });
-
-    numberS.forEach((el) => {
-        el.oninput = () => {
-            let number1 = parseFloat(numberS[0].value),
-                number2 = parseFloat(numberS[1].value);
-
-            if (number1 > number2) {
-                let tmp = number1;
-                numberS[0].value = number2;
-                numberS[1].value = tmp;
-            }
-
-            rangeS[0].value = number1;
-            rangeS[1].value = number2;
-        }
-    });
-})();
+function zoom(e){
+    var zoomer = e.currentTarget;
+    e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX
+    e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX
+    x = offsetX/zoomer.offsetWidth*100
+    y = offsetY/zoomer.offsetHeight*100
+    zoomer.style.backgroundPosition = x + '% ' + y + '%';
+  }
