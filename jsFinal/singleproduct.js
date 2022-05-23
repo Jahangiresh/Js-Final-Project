c03d686975fb5f9ac4163015c32a351640369b23
jahangir <98384389+Jahangiresh@users.noreply.github.com>
2022-05-23 23:52:38 +0400

    js final added commit 8

--- jsFinal/singleproduct.js
+++ jsFinal/singleproduct.js
@@ -6,3 +6,26 @@ function zoom(e){
     y = offsetY/zoomer.offsetHeight*100
     zoomer.style.backgroundPosition = x + '% ' + y + '%';
   }
+
+  let zoommain=document.querySelector('#zoom')
+  let zoomproduct1=document.querySelector('#zoom-product1')
+  let zoomproduct2=document.querySelector('#zoom-product2')
+  let zoomproduct3=document.querySelector('#zoom-product3')
+  let zoomproduct4=document.querySelector('#zoom-product4')
+
+zoomproduct1.addEventListener("click",function(){
+  zoommain.innerHTML=zoomproduct1.innerHTML
+zoommain.style.backgroundImage = "url(https://htmldemo.net/juan/juan/assets/img/product/product-details-img3.jpg)";
+})
+zoomproduct2.addEventListener("click",function(){
+  zoommain.innerHTML=zoomproduct2.innerHTML
+zoommain.style.backgroundImage = "url(https://htmldemo.net/juan/juan/assets/img/product/product-details-img2.jpg)";
+})
+zoomproduct3.addEventListener("click",function(){
+  zoommain.innerHTML=zoomproduct3.innerHTML
+zoommain.style.backgroundImage = "url(https://htmldemo.net/juan/juan/assets/img/product/product-details-img1.jpg)";
+})
+zoomproduct4.addEventListener("click",function(){
+  zoommain.innerHTML=zoomproduct4.innerHTML
+zoommain.style.backgroundImage = "url(https://htmldemo.net/juan/juan/assets/img/product/product-details-img4.jpg)";
+})
